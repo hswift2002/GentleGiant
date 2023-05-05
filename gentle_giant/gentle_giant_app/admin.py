f4336aa6b6b0300d539087d58de40aceafb89b99
aehrmantraut <112290446+aehrmantraut@users.noreply.github.com>
2023-05-04 20:01:51 -0500

    Leaderboard
    added leaderboard functionality with page
    
    possibly change font?

--- gentle_giant/gentle_giant_app/admin.py
+++ gentle_giant/gentle_giant_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from gentle_giant_app.models import Scoreboard
+
 # Register your models here.
+
+admin.site.register(Scoreboard)
--- gentle_giant/gentle_giant_app/urls.py
+++ gentle_giant/gentle_giant_app/urls.py
@@ -3,5 +3,7 @@ from gentle_giant_app import views
 
 urlpatterns=[
     path('', views.index, name='index'),
-    path('howto/', views.howto, name='howto')
+    path('howto/', views.howto, name='howto'),
+    path('', views.tables_view, name='gentle_giant_app'),
+    path('home/', views.home, name='table'),
 ]
