e8a384cfa650f651a0ce9155b6fd5cfc3794c7db
Skyler Skees <skyler.skees@ndsu.edu>
2023-05-04 20:43:40 -0500

    url

--- gentle_giant/gentle_giant_app/urls.py
+++ gentle_giant/gentle_giant_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns=[
     path('howto/', views.howto, name='howto'),
     path('', views.tables_view, name='gentle_giant_app'),
     path('home/', views.home, name='table'),
+    path('game/', views.game, name='game'),
 ]
